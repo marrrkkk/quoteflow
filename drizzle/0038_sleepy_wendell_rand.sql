100e5ed2f35863701a88c18a81881ae113bac353
marrrkkk <definitelynotmark13@gmail.com>
2026-04-21 07:52:39 +0800

    chore: add database schema migrations for inquiries and quotes updates

---
+++ drizzle/0038_sleepy_wendell_rand.sql
@@ -0,0 +1,4 @@
+ALTER TABLE "inquiries" ADD COLUMN "customer_phone" text;--> statement-breakpoint
+ALTER TABLE "inquiries" ADD COLUMN "company_name" text;--> statement-breakpoint
+ALTER TABLE "quotes" ADD COLUMN "customer_phone" text;--> statement-breakpoint
+ALTER TABLE "quotes" ADD COLUMN "company_name" text;
